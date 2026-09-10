fc54669e91b34741321ba12fb04f8eafcf5cf1cb
Nicholas Wilson Suwirman <113697555+nicholassuwirman@users.noreply.github.com>
2026-09-10 16:57:19 +0200

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.py
@@ -0,0 +1,8 @@
+# hash, O(n k log k)
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        groups = defaultdict(list)
+        for s in strs:
+            key = ''.join(sorted(s))
+            groups[key].append(s)
+        return list(groups.values())
